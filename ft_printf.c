4f4a391411da1ad52e6c60fb518fffe74d314bc4
Jonathan Quinde <jon.quindepaz@gmail.com>
2024-10-30 20:33:49 +0100

    feat: Añadido libreria, funcion principal Añadido estructura para el control de marcas

---
+++ ft_printf.c
@@ -0,0 +1,41 @@
+#include "printf.h" 
+
+int ft_printf(char  *str, ...)
+{
+    int i;
+    va_list arguments;
+
+    va_arg(arguments, char *);
+    i = 0;
+    while (str[i])
+    {
+        if (str[i] == '%') 
+        {
+            if (str[1] == 'c')
+                // 
+            else if (str[1] == 's')
+                //
+            else if (str[1] == 'i')
+                //
+            else if (str[1] == 'u')
+                //
+            else if (str[1] == 'p')
+                //
+            else if (str[1] == 'x')
+                //
+            else if (str[1] == 'X')
+                //
+            if else(str[1] == '%')
+            {
+                write(1, "%%", 1);
+                i++;
+            }
+        }
+        else
+        {
+            write(1, &str[i], 1);
+            i++;
+        }
+    }
+    return (i);
+}
