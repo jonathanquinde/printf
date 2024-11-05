6ab591b2e0844fa09eb49a522933612ccaba4d24
Jonathan Quinde <jon.quindepaz@gmail.com>
2024-11-05 18:51:06 +0100

    refactor: condicion de encuentor de marcas movido a una funcion aparte fix: para inicializar arguments use va_arg en ves de va_start fix: bucle infinito al iterar sobre str debido a i solo incrementando dentro de un else fix: estructura mal puesta style: conversion_specifiers, renombre a variable que contabiliza los caracateres printeados a sum

--- ft_printf.c
+++ ft_printf.c
@@ -3,39 +3,43 @@
 int ft_printf(char  *str, ...)
 {
     int i;
+    int sum;
     va_list arguments;
 
-    va_arg(arguments, char *);
+    va_start(arguments, str);
+    sum = 0;
     i = 0;
     while (str[i])
     {
-        if (str[i] == '%') 
-        {
-            if (str[1] == 'c')
-                put_char(va_arg(arguments, char));
-            else if (str[1] == 's')
-                put_str(va_arg(arguments, char *));
-            else if (str[1] == 'i')
-                //
-            else if (str[1] == 'u')
-                //
-            else if (str[1] == 'p')
-                //
-            else if (str[1] == 'x')
-                //
-            else if (str[1] == 'X')
-                //
-            if else(str[1] == '%')
-            {
-                write(1, "%%", 1);
-                i++;
-            }
-        }
+        if (str[i++] == '%') 
+            sum += conversion_specifiers(str[1], arguments);
         else
-        {
             write(1, &str[i], 1);
-            i++;
-        }
+        i++;
     }
     return (i);
+}
+
+int conversion_specifiers(char  word, va_list arguments)
+{
+    size_t  sum;
+    
+    sum = 0;
+    if (word == 'c')
+        sum += put_char(va_arg(arguments, char));
+    else if (word == 's')
+        sum += put_str(va_arg(arguments, char *));
+    else if (word = 'i')
+        //
+    else if (word == 'u')
+        //
+    else if (word == 'p')
+        //
+    else if (word == 'x')
+        //
+    else if (word == 'X')
+        //
+    else if (word == '%')
+        sum += write(1, "%%", 1);
+    return (sum);
 }
