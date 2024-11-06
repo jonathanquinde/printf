382e0135882e2851c125cfcf7c81289a7db2f384
Jonathan Quinde <jon.quindepaz@gmail.com>
2024-11-06 18:07:00 +0100

    fix: en caso de str NULL, put_str printeaba (nil). Ahora printea  (null)

--- char_str_handler.c
+++ char_str_handler.c
@@ -10,7 +10,7 @@ int put_str(char *str)
     int i;
 
     if (str == NULL)
-        return (write(1, "(nil)", 5));
+        return (write(1, "(null)", 6));
     i = 0;
     while (*str)
         i += write(1, str, 1);
