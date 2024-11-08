77ca254bdc3774dec844bbbc0a9353d3d19421ab
jonathanquinde <jonquindepaz@gmail.com>
2024-11-08 14:48:34 +0100

    test: god

--- test.c
+++ test.c
@@ -26,14 +26,14 @@ int	main(void)
 	printf(MAG "char normal: a" RESET "\n");
 	char    c = 'a';
 	p_result = printf("%c\n", c);
-	u_result = printf("%c\n", c);
+	u_result = ft_printf("%c\n", c);
 	printf(CYN "Printf: %d" RESET "\n" GRN "ft_printf: %d" RESET "\n", p_result, u_result);
 	printf("\n");
 
 	printf(MAG "char overflow: a + 256" RESET "\n");
 	int     c1 = 'a' + 256;
 	p_result = printf("%c\n", c1);
-	u_result = printf("%c\n", c1);
+	u_result = ft_printf("%c\n", c1);
 	printf(CYN "Printf: %d" RESET "\n" GRN "ft_printf: %d" RESET "\n", p_result, u_result);
 
 	//2 - Prueba con strings
@@ -41,21 +41,21 @@ int	main(void)
 	printf(MAG "str normal" RESET "\n");
 	char *str = "pato";
 	p_result = printf("%s\n", str);
-	u_result = printf("%s\n", str);
+	u_result = ft_printf("%s\n", str);
 	printf(CYN "Printf: %d" RESET "\n" GRN "ft_printf: %d" RESET "\n", p_result, u_result);
 	printf("\n");
 
 	printf(MAG "str vacia" RESET "\n");
 	str = "";
 	p_result = printf("%s\n", str);
-	u_result = printf("%s\n", str);
+	u_result = ft_printf("%s\n", str);
 	printf(CYN "Printf: %d" RESET "\n" GRN "ft_printf: %d" RESET "\n", p_result, u_result);
 	printf("\n");
 
 	printf(MAG "str NULL" RESET "\n");
 	str = NULL;
 	p_result = printf("%s\n", str);
-	u_result = printf("%s\n", str);
+	u_result = ft_printf("%s\n", str);
 	printf(CYN "Printf: %d" RESET "\n" GRN "ft_printf: %d" RESET "\n", p_result, u_result);
 
 	//3 - Prueba con numeros
@@ -63,56 +63,56 @@ int	main(void)
 	printf(MAG "numero: 0" RESET "\n");
 	int	n = 0;
 	p_result = printf("%d\n", n);
-	u_result = printf("%d\n", n);
+	u_result = ft_printf("%d\n", n);
 	printf(CYN "Printf: %d" RESET "\n" GRN "ft_printf: %d" RESET "\n", p_result, u_result);
 	printf("\n");
 
 	printf(MAG "numero: 5" RESET "\n");
 	n = 5;
 	p_result = printf("%d\n", n);
-	u_result = printf("%d\n", n);
+	u_result = ft_printf("%d\n", n);
 	printf(CYN "Printf: %d" RESET "\n" GRN "ft_printf: %d" RESET "\n", p_result, u_result);
 	printf("\n");
 
 	printf(MAG "numero: 100" RESET "\n");
 	n = 100;
 	p_result = printf("%d\n", n);
-	u_result = printf("%d\n", n);
+	u_result = ft_printf("%d\n", n);
 	printf(CYN "Printf: %d" RESET "\n" GRN "ft_printf: %d" RESET "\n", p_result, u_result);
 	printf("\n");
 
 	printf(MAG "numero max: 2147483647" RESET "\n");
 	n = 2147483647;
 	p_result = printf("%d\n", n);
-	u_result = printf("%d\n", n);
+	u_result = ft_printf("%d\n", n);
 	printf(CYN "Printf: %d" RESET "\n" GRN "ft_printf: %d" RESET "\n", p_result, u_result);
 	printf("\n");
 
 	printf(MAG "numero negativo: -1" RESET "\n");
 	n = -1;
 	p_result = printf("%d\n", n);
-	u_result = printf("%d\n", n);
+	u_result = ft_printf("%d\n", n);
 	printf(CYN "Printf: %d" RESET "\n" GRN "ft_printf: %d" RESET "\n", p_result, u_result);
 	printf("\n");
 
 	printf(MAG "numero negativo: -5" RESET "\n");
 	n = -5;
 	p_result = printf("%d\n", n);
-	u_result = printf("%d\n", n);
+	u_result = ft_printf("%d\n", n);
 	printf(CYN "Printf: %d" RESET "\n" GRN "ft_printf: %d" RESET "\n", p_result, u_result);
 	printf("\n");
 
 	printf(MAG "numero negativo: -100" RESET "\n");
 	n = -100;
 	p_result = printf("%d\n", n);
-	u_result = printf("%d\n", n);
+	u_result = ft_printf("%d\n", n);
 	printf(CYN "Printf: %d" RESET "\n" GRN "ft_printf: %d" RESET "\n", p_result, u_result);
 	printf("\n");
 
 	printf(MAG "numero min: -2147483648" RESET "\n");
 	n = -2147483648;
 	p_result = printf("%d\n", n);
-	u_result = printf("%d\n", n);
+	u_result = ft_printf("%d\n", n);
 	printf(CYN "Printf: %d" RESET "\n" GRN "ft_printf: %d" RESET "\n", p_result, u_result);
 	printf("\n");
 
@@ -121,49 +121,71 @@ int	main(void)
 	printf(MAG "numero: 0" RESET "\n");
 	unsigned int x = 0;
 	p_result = printf("%x\n", x);
-	u_result = printf("%x\n", x);
+	u_result = ft_printf("%x\n", x);
 	printf(CYN "Printf: %d" RESET "\n" GRN "ft_printf: %d" RESET "\n", p_result, u_result);
 	printf("\n");
 
 	printf(MAG "numero: 10" RESET "\n");
 	x = 10;
 	p_result = printf("%x\n", x);
-	u_result = printf("%x\n", x);
+	u_result = ft_printf("%x\n", x);
 	printf(CYN "Printf: %d" RESET "\n" GRN "ft_printf: %d" RESET "\n", p_result, u_result);
 	printf("\n");
 
 	printf(MAG "numero: 1000" RESET "\n");
 	x = 1000;
 	p_result = printf("%x\n", x);
-	u_result = printf("%x\n", x);
+	u_result = ft_printf("%x\n", x);
 	printf(CYN "Printf: %d" RESET "\n" GRN "ft_printf: %d" RESET "\n", p_result, u_result);
 	printf("\n");
 
 	printf(MAG "numero largo: 2147483647" RESET "\n");
 	x = 2147483647;
 	p_result = printf("%x\n", x);
-	u_result = printf("%x\n", x);
+	u_result = ft_printf("%x\n", x);
 	printf(CYN "Printf: %d" RESET "\n" GRN "ft_printf: %d" RESET "\n", p_result, u_result);
 	printf("\n");
 
 	printf(MAG "numero max: 4294967295" RESET "\n");
 	x = 4294967295;
 	p_result = printf("%x\n", x);
-	u_result = printf("%x\n", x);
+	u_result = ft_printf("%x\n", x);
 	printf(CYN "Printf: %d" RESET "\n" GRN "ft_printf: %d" RESET "\n", p_result, u_result);
 	printf("\n");
 
 	printf(MAG "underflow: -1" RESET "\n");
 	int x1 = -1;
 	p_result = printf("%x\n", x1);
-	u_result = printf("%x\n", x1);
+	u_result = ft_printf("%x\n", x1);
 	printf(CYN "Printf: %d" RESET "\n" GRN "ft_printf: %d" RESET "\n", p_result, u_result);
 	printf("\n");
 
 	printf(MAG "underflow: -2147483648" RESET "\n");
 	x1 = -2147483648;
 	p_result = printf("%x\n", x1);
-	u_result = printf("%x\n", x1);
+	u_result = ft_printf("%x\n", x1);
+	printf(CYN "Printf: %d" RESET "\n" GRN "ft_printf: %d" RESET "\n", p_result, u_result);
+	printf("\n");
+
+	//5 - Prueba con porcentaje
+	printf(YEL "---5: Prueba con porcentaje---\n" RESET);
+	p_result = printf("%%\n");
+	u_result = ft_printf("%%\n");
+	printf(CYN "Printf: %d" RESET "\n" GRN "ft_printf: %d" RESET "\n", p_result, u_result);
+	printf("\n");
+
+	//6 - Prueba con direcciones de memoria
+	printf(YEL "---6: Prueba con direcciones de memoria---\n" RESET);
+	printf(MAG "normal" RESET "\n");
+	int pato = 2;
+	p_result = printf("%p\n", &pato);
+	u_result = ft_printf("%p\n", &pato);
+	printf(CYN "Printf: %d" RESET "\n" GRN "ft_printf: %d" RESET "\n", p_result, u_result);
+	printf("\n");
+
+	printf(MAG "normal" RESET "\n");
+	p_result = printf("%p\n", NULL);
+	u_result = ft_printf("%p\n", NULL);
 	printf(CYN "Printf: %d" RESET "\n" GRN "ft_printf: %d" RESET "\n", p_result, u_result);
 	printf("\n");
 }
