88397eae8c228db7e4dff7e9eb42b0b331ba137c
jonathanquinde <jonquindepaz@gmail.com>
2024-11-08 18:56:52 +0100

    fix: esto iba en el anterio commit jejeje

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: jquinde- < jquinde-@student.42madrid.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 17:43:03 by jquinde-          #+#    #+#             */
-/*   Updated: 2024/11/08 13:04:59 by jquinde-         ###   ########.fr       */
+/*   Updated: 2024/11/08 18:56:35 by jquinde-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 # define BASE_HEX_MAY "0123456789ABCDEF"
 # define BASE_DEC "0123456789"
 
-int	ft_printf(char *str, ...);
+int	ft_printf(char const *str, ...);
 int	put_char(char c);
 int	put_str(char *str);
 int	put_nbr_base(unsigned long num, char *base, size_t len);
